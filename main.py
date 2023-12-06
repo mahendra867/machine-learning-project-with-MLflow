5db0dcbe4f648d1b4a3bbcbe7bd7fce25f706a23
mahendra867 <mahendramahesh2001@gmail>
2023-12-06 20:04:05 +0530

    till so far

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.mlproject_with_mlflow import logger  # although i have src folder when you click the explorar we can see that src but iam directly calling this mlproject_with_mlflow because i have initlizied my login functionality inside the __init__.py constructor thats why i dont need to call this src seperatly  you can call src folder by like this from src.mlproject_with_mlflow import logger , but if we want to ingore a folder like this to import that src folder , we can mention that inside the __init__.py constructor to ignore of calling the src folder 
+
+
+logger.info('welcome to our custom logging')
